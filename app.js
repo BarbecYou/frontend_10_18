8cff09c4ef4fc0c5317f77c3d9e6c679f9673992
Füredi Dániel <daniel.furedi03@gmail.com>
2022-10-23 13:26:09 +0200

    boilerplate

---
+++ app.js
@@ -0,0 +1,40 @@
+const data = [
+    {
+      "nev": "Maxi Ultra",
+      "meret": "128"
+    },
+    {
+      "nev": "Maxi Ultra S",
+      "meret": "256"
+    },
+    {
+      "nev": "Maxi Ultra X",
+      "meret": "512"
+    },
+    {
+      "nev": "Átlagos SD kártya",
+      "meret": "128"
+    },
+    {
+      "nev": "Átlagos SD kártya 2",
+      "meret": "256"
+    },
+    {
+      "nev": "Átlagos SD kártya 2.1",
+      "meret": "256"
+    },
+    {
+      "nev": "Ólcsó microSD",
+      "meret": "32"
+    },
+    {
+      "nev": "Kevésbé olcsó microSD",
+      "meret": "64"
+    }
+  ];
+
+  document.addEventListener('DOMContentLoaded', init);
+
+  function init(){
+    document.getElementById('searchTextInput').addEventListener()
+  };
